d7ef5607175e9fa981f27bd9072ad3672c7309d1
Prashant23452 <prashantkumarjha.cse2022@nsec.ac.in>
2024-06-07 08:49:40 +0530

    15

--- script.js
+++ script.js
@@ -23,3 +23,45 @@ const inputClearer = () => {
       inputClearer();
       return;
     }
+
+    // Convert height to meters
+  h /= 100.0;
+
+  // Calculate BMI
+  let bmi = w / (h * h);
+
+  // Round BMI to two decimal places
+  bmi = parseFloat(bmi).toFixed(2);
+
+  let img;
+  let data = "";
+
+  // Determine the BMI category and corresponding image
+  if (bmi < 18.5) {
+    data = "You Are Underweight";
+    img = "./assets/underweight.jpg";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    data = "You Are Healthy";
+    img = "./assets/healthy.jpg";
+  } else if (bmi >= 25 && bmi < 30) {
+    data = "You Are Overweight";
+    img = "./assets/overweight.jpg";
+  } else if (bmi >= 30) {
+    data = "You Are Obese";
+    img = "./assets/obese.jpg";
+  } else {
+    data = "Please Enter a Valid Input";
+  }
+
+  // Set the source attribute of the 'body' element to display the appropriate image
+  document.getElementById("body").setAttribute("src", img);
+
+  // Display the result and BMI information
+  document.getElementById("res").innerHTML = ` ${data}.`;
+  document.getElementById(
+    "result"
+  ).innerHTML = `Your BMI is <strong>${bmi}</strong>.`;
+
+  // Show the 'info' card
+  document.getElementById("info").setAttribute("class", "card show");
+});
